72144b556dde6aac0d0f90f9d9e67b97d1e44d12
thebestpoliticaltest <82084941+thebestpoliticaltest@users.noreply.github.com>
2021-04-07 10:14:33 +0200

    !!!
    !!!

---
+++ questions.js
@@ -0,0 +1,6 @@
+questions =  [
+    {"id": 0, "question": "I support a good ideologie.", "effects": {"right": 1}},
+    {"id": 1, "question": "I support a bad ideologie.", "effects": {"auth": -1}},
+    {"id": 2, "question": "I support a based ideologie", "effects": {"prog": 1}},
+    {"id": 3, "question": "I support a cringe ideologie.", "effects": {"prog": -1}},
+];
